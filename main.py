42fd324dc36b95ddca4d837de9f3e060530495eb
SAHIL NAZIR <74105412+lonesahilnazir@users.noreply.github.com>
2025-01-28 15:34:19 +0000

    Week 2 - GrPA 1

--- main.py
+++ main.py
@@ -1,24 +1,21 @@
-age = int(input()) # int: Read a number as integer from standard input
-dob = str(input()) # str: Read a string of format dd/mm/yy from standard input
-day, month, year = int(dob[:2]), int(dob[3:5]), int(dob[-2:]) # int, int, int: Get the correct parts from dob as int
+x1 = input()
+x2 = input()
+y1 = input()
+y2 = input()
+y3 = input()
+z = input()
 
-fifth_birthday = f"{day}/{month}/{year+5}" # str: fifth birthday formatted as day/month/year 
-
-last_birthday = f"{day}/{month}/{year+age}" # str: last birthday formatted as day/month/year
-# day, month, year = 12,7,12
-adding_10_m = month + 10 - 1
-month = (adding_10_m % 12) + 1
-year = year + (adding_10_m // 12)
-
-tenth_month = f"{day}/{month}/{year}" # str: dob same day after 10 months formatted as day/month/year
-
-# print tenth_month, fifth_birthday and last_birthday in same line separated by comma and a space
-print(tenth_month, fifth_birthday, last_birthday, sep=", ")
-
-weight = float(input()) # float: Read a number as float from stdin(Standard input)
-
-weight_readable = f"{int(weight // 1)} kg {int((weight - (weight // 1))*1000)} grams" # str: reformat weight of format 55 kg 250 grams
-
-# print weight_readable 
-print(weight_readable)
-# print(adding_10_m // 12, month)
+# swap the values of `x1` and `x2`
+x1, x2 = x2, x1
+# do a circular swap of `y1`, `y2` and `y3`  like y1 = y2, y2 = y3, y3 = y1 
+y1, y2, y3 = y2, y3, y1
+# create a new variable `a` with the value of `z`
+a = z
+# delete the variable `z`
+del z
+print(x1)
+print(x2)
+print(y1)
+print(y2)
+print(y3)
+print(a)
