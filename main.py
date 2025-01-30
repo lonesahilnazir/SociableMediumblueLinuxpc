44f8dc00185a5339a2ea1042b833e87df755abe1
SAHIL NAZIR <74105412+lonesahilnazir@users.noreply.github.com>
2025-01-30 14:14:48 +0000

    Reverse a number and print sum of all digits

--- main.py
+++ main.py
@@ -1,28 +1,7 @@
-main_dish = input()
-time_of_day = int(input())
-has_voucher =   input().lower() == "true"
-is_card_payment = input().lower() == "true"
-
-if main_dish == "paneer tikka":
-    cost = 250
-elif main_dish == "butter chicken":
-    cost = 240
-elif main_dish == "masala dosa":
-    cost = 200
-else: # if main dish is invalid print invalid dish and exit the code.
-   print("Invalid main dish")
-   exit() 
-
-if time_of_day >= 12 and time_of_day <= 15:
-    total_cost = (1 - 0.15) * cost
-else:
-    total_cost = cost
-
-if has_voucher:
-    total_cost *= 0.9  # Apply voucher discount
-
-if is_card_payment:  # service charge for card payments
-    service_charge = 0.05 * total_cost
-    total_cost += service_charge
-
-print(f"{total_cost:.02f}")
+num = int(input("Enter Number: "))
+str = str(num)
+print(str[::-1])
+total = 0
+for s in str:
+  total += int(s)
+print(total)
